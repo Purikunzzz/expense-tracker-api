d5aa160b435334a3768518881f9b28583d9898ad
Purikun <67991019@kmitl.ac.th>
2026-08-12 14:03:21 +0700

    fix: run migration in asyncio.to_thread on startup

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.routers import auth, category, expense, summary
+from alembic.config import Config
+from alembic import command
+import asyncio
+
+def run_migrations():
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await asyncio.to_thread(run_migrations)
+    yield
 
 app = FastAPI(
     title="Expense Tracker API",
-    version="0.1.0"
+    version="0.1.0",
+    lifespan=lifespan
 )
 
 app.include_router(auth.router)
